1ec8239bedd1e037ec8f7ea7df4d7f8d4d61685f
vidalperezbohoyo <v.perezb.2019@alumnos.urjc.es>
2022-02-08 16:55:06 +0100

    Starting with curvature

--- practica1_pkg/include/practica1_pkg/WallFollower.hpp
+++ practica1_pkg/include/practica1_pkg/WallFollower.hpp
@@ -1,92 +1,58 @@
 #include <memory>
 
 #include "lifecycle_msgs/msg/state.hpp"
-#include "std_msgs/msg/float64.hpp"
+#include "geometry_msgs/msg/twist.hpp" 
+#include "sensor_msgs/msg/laser_scan.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 
+#include "practica1_pkg/marker_arrow.hpp"
+
 using rcl_interfaces::msg::ParameterType;
 using std::placeholders::_1;
+using namespace std::chrono_literals;
 
-class WallFollower : public rclcpp_lifecycle::LifecycleNode
-{
+class WallFollower : public rclcpp_lifecycle::LifecycleNode {
+
+  enum State {
+    OBSTACLE = 0,
+    FREE_WAY
+  };
+
+
 public:
-  WallFollower()
-  : rclcpp_lifecycle::LifecycleNode("lifecycle_node_example")
-  {
-    declare_parameter("speed", 0.34);
-    pub_ = create_publisher<std_msgs::msg::Float64>("configured_speed", 100);
-  }
-
-  using CallbackReturnT =
-    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
-
-  CallbackReturnT on_configure(const rclcpp_lifecycle::State & state)
-  {
-    RCLCPP_INFO(get_logger(), "[%s] Configuring from [%s] state...", get_name(), state.label().c_str());
-   
-    speed_ = get_parameter("speed").get_value<double>();
-   
-    return CallbackReturnT::SUCCESS;
-  }
-
-  CallbackReturnT on_activate(const rclcpp_lifecycle::State & state) 
-  {
-    RCLCPP_INFO(get_logger(), "[%s] Activating from [%s] state...", get_name(), state.label().c_str());
-    
-    pub_->on_activate();
-    
-    return CallbackReturnT::SUCCESS;
-  }
-
-  CallbackReturnT on_deactivate(const rclcpp_lifecycle::State & state) 
-  {
-    RCLCPP_INFO(get_logger(), "[%s] Deactivating from [%s] state...", get_name(), state.label().c_str());
-    
-    pub_->on_deactivate();
-    
-    return CallbackReturnT::SUCCESS;
-  }
-
-  CallbackReturnT on_cleanup(const rclcpp_lifecycle::State & state) 
-  {
-    RCLCPP_INFO(get_logger(), "[%s] Cleanning Up from [%s] state...", get_name(), state.label().c_str());
-    
-    pub_.reset();
-
-    return CallbackReturnT::SUCCESS;
-  }
-
-  CallbackReturnT on_shutdown(const rclcpp_lifecycle::State & state) 
-  {
-    RCLCPP_INFO(get_logger(), "[%s] Shutting Down from [%s] state...", get_name(), state.label().c_str());
-    
-    pub_.reset();
-    
-    return CallbackReturnT::SUCCESS;
-  }
-
-    CallbackReturnT on_error(const rclcpp_lifecycle::State & state) 
-  {
-    RCLCPP_INFO(get_logger(), "[%s] Shutting Down from [%s] state...", get_name(), state.label().c_str());
-    return CallbackReturnT::SUCCESS;
-  }
   
-  void do_work() 
-  {
-    if (pub_->is_activated()) {
-      std_msgs::msg::Float64 msg;
-      msg.data = speed_;
+  WallFollower(); 
+       
+  using CallbackReturnT = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
-      pub_->publish(msg);
-    }
-  }
+  CallbackReturnT on_configure(const rclcpp_lifecycle::State & state);
 
+  CallbackReturnT on_activate(const rclcpp_lifecycle::State & state);
+
+  CallbackReturnT on_deactivate(const rclcpp_lifecycle::State & state);
+
+  CallbackReturnT on_cleanup(const rclcpp_lifecycle::State & state);
+
+  CallbackReturnT on_shutdown(const rclcpp_lifecycle::State & state);
+
+  CallbackReturnT on_error(const rclcpp_lifecycle::State & state);
+
+  void behaviour();
+
+  void turn_right();
+
+  void move_in_a_curve();
+
+protected:
+  void laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
 
 private:
-  double speed_;
-  rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::Float64>::SharedPtr pub_;
-};
-
+  rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::Twist>::SharedPtr velocity_pub_; // Intelligent pointer to a velocity publisher.
+  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  State state_ = FREE_WAY;
+};  
 
---
+++ practica1_pkg/src/practica1_pkg/WallFollower.cpp
@@ -0,0 +1,94 @@
+#include "practica1_pkg/WallFollower.hpp"
+
+WallFollower::WallFollower() : rclcpp_lifecycle::LifecycleNode("follower_node") {
+  velocity_pub_ = create_publisher<geometry_msgs::msg::Twist>("nav_vel", 100);
+  laser_sub_ = create_subscription<sensor_msgs::msg::LaserScan>("scan_raw", 1, std::bind(&Follower::laser_callback, this, _1));
+}
+
+using CallbackReturnT = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+CallbackReturnT WallFollower::on_configure(const rclcpp_lifecycle::State& state) {
+  RCLCPP_INFO(get_logger(), "[%s] Configuring from [%s] state...", get_name(), state.label().c_str());
+
+  return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT WallFollower::on_activate(const rclcpp_lifecycle::State& state) {
+  RCLCPP_INFO(get_logger(), "[%s] Activating from [%s] state...", get_name(), state.label().c_str());
+
+  velocity_pub_->on_activate();
+  timer_ = create_wall_timer{50ms, std::bind(&WallFollower::behaviour, this)};
+
+  return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT WallFollower::on_deactivate(const rclcpp_lifecycle::State& state) {
+  RCLCPP_INFO(get_logger(), "[%s] Deactivating from [%s] state...", get_name(), state.label().c_str());
+
+  velocity_pub_->on_deactivate();
+  timer_ = nullptr;
+
+  return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT WallFollower::on_cleanup(const rclcpp_lifecycle::State& state) {
+  RCLCPP_INFO(get_logger(), "[%s] Cleanning Up from [%s] state...", get_name(), state.label().c_str());
+
+  velocity_pub_.reset();
+
+  return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT WallFollower::on_shutdown(const rclcpp_lifecycle::State& state) {
+  RCLCPP_INFO(get_logger(), "[%s] Shutting Down from [%s] state...", get_name(), state.label().c_str());
+
+  velocity_pub_.reset();
+
+  return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT WallFollower::on_error(const rclcpp_lifecycle::State& state) {
+  RCLCPP_INFO(get_logger(), "[%s] Shutting Down from [%s] state...", get_name(), state.label().c_str());
+  return CallbackReturnT::SUCCESS;
+}
+
+void WallFollower::turn_right() {
+  geometry_msgs::msg::Twist msg;
+  msg.linear.x = 0.0;
+  msg.angular.z = -0.1;
+  velocity_pub_->publish(msg); 
+}
+
+void WallFollower::move_in_a_curve() {
+  geometry_msgs::msg::Twist msg;
+  msg.linear.x = 0.2;
+  msg.angular.z = -0.01;
+  velocity_pub_->publish(msg);
+}
+
+void WallFollower::behaviour() {
+  if (state_ == OBSTACLE) {
+    turn_right();
+  } else if (state_ == FREE_WAY) {
+    move_in_a_curve();
+  }
+}
+
+void WallFollower::laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+
+  int CONE_ANGLE = 0.1;
+
+  int cone_start = int(((CONE_ANGLE/2) - msg->angle_min)/ msg->angle_increment) ;
+  int cone_end = int(((-CONE_ANGLE/2) - msg->angle_min)/ msg->angle_increment) ;
+
+  float min_distance = *std::min_element(msg->ranges[cone_start], msg->ranges[cone_end]);
+
+  float OBSTACLE_DISTANCE = 0.30;
+
+  if (min_distance < OBSTACLE_DISTANCE) {
+    state_ = OBSTACLE;
+  } else {
+    state_ = FREE_WAY;
+  }
+
+}
